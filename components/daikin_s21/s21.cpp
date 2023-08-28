6066b74a02393c0021c7fd34de5cea101040531d
D Fischer <mail@daanfischer.nl>
2023-08-28 19:49:05 +0200

    Enable all experiments codes

--- components/daikin_s21/s21.cpp
+++ components/daikin_s21/s21.cpp
@@ -384,13 +384,13 @@ void DaikinS21::update() {
 
 #ifdef S21_EXPERIMENTS
   ESP_LOGD(TAG, "** UNKNOWN QUERIES **");
-  // auto experiments = {"F2", "F3", "F4", "F8", "F9", "F0", "FA", "FB", "FC",
-  //                     "FD", "FE", "FF", "FG", "FH", "FI", "FJ", "FK", "FL",
-  //                     "FM", "FN", "FO", "FP", "FQ", "FR", "FS", "FT", "FU",
-  //                     "FV", "FW", "FX", "FY", "FZ"};
+  auto experiments = {"F2", "F3", "F4", "F8", "F9", "F0", "FA", "FB", "FC",
+                       "FD", "FE", "FF", "FG", "FH", "FI", "FJ", "FK", "FL",
+                       "FM", "FN", "FO", "FP", "FQ", "FR", "FS", "FT", "FU",
+                       "FV", "FW", "FX", "FY", "FZ"};
   // Observed BRP device querying these.
-  std::vector<std::string> experiments = {"F2", "F3", "F4", "RN",
-                                          "RX", "RD", "M",  "FU0F"};
+  //std::vector<std::string> experiments = {"F2", "F3", "F4", "RN",
+  //                                        "RX", "RD", "M",  "FU0F"};
   this->run_queries(experiments);
 #endif
 }
